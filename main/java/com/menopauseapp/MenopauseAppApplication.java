1091ed397a83016794057719e34b339a2a67fa07
hehe7653 <yashvi2008jain@gmail.com>
2025-04-20 17:06:50 +0530

    Add files via upload

---
+++ main/java/com/menopauseapp/MenopauseAppApplication.java
@@ -0,0 +1,11 @@
+package com.menopauseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenopauseAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MenopauseAppApplication.class, args);
+    }
+}
